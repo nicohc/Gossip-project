ca61ef5e40038cbac88e6cf4404abf20b503e9aa
Nico <nico1099@hotmail.fr>
2018-01-31 18:58:25 +0100

    Site de gossips fonctionnel

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,5 +1,9 @@
 class GossipsController < ApplicationController
 
+  def index
+    @gossips= Gossip.all
+  end
+
   def new
     @gossip= Gossip.new
   end
@@ -15,6 +19,25 @@ class GossipsController < ApplicationController
 
   def show
     @gossip = Gossip.find(params[:id])
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+        @gossip = Gossip.find(params[:id])
+        if @gossip.update(gossip_params)
+          redirect_to @gossip
+        else
+          render 'edit'
+        end
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to gossips_path
   end
 
   private
--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,2 +1,8 @@
 class Gossip < ApplicationRecord
+
+  validates :anonymous_author, presence: true,
+                      length: { minimum: 3 }
+  validates :content, presence: true,
+                      length: { minimum: 5 }
+
 end
