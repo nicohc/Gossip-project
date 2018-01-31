a3b2ae8031f01cfc8f6231901087a97c9a4c84e1
Nico <nico1099@hotmail.fr>
2018-01-31 17:17:47 +0100

    Gossips ajoutables

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,2 +1,26 @@
 class GossipsController < ApplicationController
+
+  def new
+    @gossip= Gossip.new
+  end
+
+  def create
+    @gossip = Gossip.new(gossip_params)
+    if  @gossip.save
+      redirect_to @gossip
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
+
+  private
+    def gossip_params
+      params.permit(:anonymous_author, :content)
+    end
+
+
 end
